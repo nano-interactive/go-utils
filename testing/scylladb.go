9ec1349ffeec38a829499a7c2f289c96cb6b1401
Ignjat Jokanovic <jokanovic.ignjat@gmail.com>
2022-12-01 16:20:09 +0100

    Removed keyspace option

--- testing/scylladb.go
+++ testing/scylladb.go
@@ -9,35 +9,27 @@ import (
 	"github.com/gocql/gocql"
 )
 
-
 type ScyllaDBOptions interface {
 	GetOptions() *gocql.ClusterConfig
 }
 
-
 func getScyllaDBConfig(t *testing.T, opt *gocql.ClusterConfig) *gocql.ClusterConfig {
 	t.Helper()
-
-	keyspace := "testing_keyspace"
 
 	if value, exists := os.LookupEnv("SCYLLADB_HOSTS"); exists {
 		opt.Hosts = strings.Split(value, ",")
 	}
 
 	if value, exists := os.LookupEnv("SCYLLADB_KEYSPACE"); exists {
-		keyspace = value
+		opt.Keyspace = value
 	}
-
 
 	if value, exists := os.LookupEnv("SCYLLADB_PORT"); exists {
 		opt.Port, _ = strconv.Atoi(value)
 	}
 
-	opt.Keyspace = keyspace // FIXME: + "_" + strconv.FormatUint(uint64(rand.Int31n(100_000)), 10)
-
 	return opt
 }
-
 
 func CreateScyllaDB(t *testing.T, optMaker ScyllaDBOptions) *gocql.Session {
 	t.Helper()
