1c327b1a933824b8c7621c21acb3b1111ae4ccab
Stefan Bogdanovic <sbogdanovic@nanointeractive.com>
2025-06-06 13:05:40 +0200

    feat: null bool validator implemented
    Signed-off-by: Stefan Bogdanovic <sbogdanovic@nanointeractive.com>

---
+++ validators/null_bool.go
@@ -0,0 +1,17 @@
+package validators
+
+import "errors"
+
+// NullBoolValidator validates if a value is boolean pointer.
+// Useful for checking if a variable exists in configuration or not.
+type NullBoolValidator struct {
+}
+
+func (r NullBoolValidator) Validate(value any) error {
+	ptr := value.(*bool)
+	if ptr == nil {
+		return errors.New("boolean value is required")
+	}
+
+	return nil
+}
---
+++ validators/null_bool_test.go
@@ -0,0 +1,49 @@
+package validators_test
+
+import (
+	"errors"
+	"github.com/nano-interactive/go-utils/v2/validators"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNullBoolValidator(t *testing.T) {
+	validator := validators.NullBoolValidator{}
+
+	tests := []struct {
+		name     string
+		input    *bool
+		expected error
+	}{
+		{
+			name:     "Valid true value",
+			input:    boolPtr(true),
+			expected: nil,
+		},
+		{
+			name:     "Valid false value",
+			input:    boolPtr(false),
+			expected: nil,
+		},
+		{
+			name:     "Nil pointer",
+			input:    nil,
+			expected: errors.New("boolean value is required"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validator.Validate(tt.input)
+			if tt.expected == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, tt.expected.Error())
+			}
+		})
+	}
+}
+
+func boolPtr(b bool) *bool {
+	return &b
+}
